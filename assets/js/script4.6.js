8ef16bc7926be973818271e0d146c6f30a2ae48b
kardwane <kardwane@gmail.com>
2022-04-26 09:38:59 +0200

    ajout exo4.6

---
+++ assets/js/script4.6.js
@@ -0,0 +1,40 @@
+const score1 = document.querySelector("#score1");
+const score2 = document.querySelector("#score2");
+const score3 = document.querySelector("#score3");
+const score4 = document.querySelector("#score4");
+const check = document.querySelector("#check");
+
+check.addEventListener("click", () => {
+  // ballotage
+  if (
+    score1.value >= 12.5 &&
+    score1.value < 50 &&
+    score2.value < 50 &&
+    score3.value < 50 &&
+    score4.value < 50
+  ) {
+    // favorable?
+    if (
+      score1.value > score2.value &&
+      score1.value > score3.value &&
+      score1.value > score4.value
+    ) {
+      alert("ballotage favorable");
+    } else {
+      alert("ballotage défavorable");
+    }
+  } else {
+    //gagnant ou battu
+    if (score1.value > 50) {
+      alert("élu au premier tour");
+    }
+    if (
+      score2.value > 50 ||
+      score3.value > 50 ||
+      score4.value > 50 ||
+      score1.value < 12.5
+    ) {
+      alert("battu au premier tour");
+    }
+  }
+});
